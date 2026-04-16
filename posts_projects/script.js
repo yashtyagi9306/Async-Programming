c34b274feb45ad2484f51c235f9c02f47e2cccde
yashtyagi9306 <yashu66556655@gmail.com>
2026-04-16 21:55:26 +0530

    first async project. applied the various functions promise await and callback and learnt the flow of program

---
+++ posts_projects/script.js
@@ -0,0 +1,81 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const postsContainer = document.querySelector(".posts-container");
+//     const APIUrl = "https://jsonplaceholder.typicode.com/posts";
+//     async function fetchPosts(){
+//         try {
+//             //request
+//             const response = await fetch(APIUrl);
+//             const posts = await response.json();
+//             postsContainer.innerHTML = "";
+//             posts.forEach((post) => {
+//                 const postElement = createPostElement(post);
+//                 postsContainer.appendChild(postElement);                
+                
+//             });
+            
+//         } catch(error){
+//             postsContainer.innerHTML = '<p>Error loading posts</p>'
+//         }
+//     }
+
+//     function createPostElement(post){
+//         const article = document.createElement("article");
+//         article.className = "post-card";
+
+//         const title = document.createElement("h2");
+//         title.className = "post-title";
+//         title.textContent = post.title;
+
+//         const body = document.createElement("p");
+//         body.className = "post-body";
+//         body.textContent = post.body;
+
+//         article.appendChild(title);
+//         article.appendChild(body);
+
+//         return article;
+//     fetchPosts();
+// });
+
+//Make sure all assets are loaded
+document.addEventListener("DOMContentLoaded", () => {
+    const postsContainer = document.querySelector(".posts-container");
+    const APIUrl = "https://jsonplaceholder.typicode.com/posts";
+
+    async function fetchPosts(){
+        try {
+            const response = await fetch(APIUrl);
+            const posts = await response.json();
+
+            postsContainer.innerHTML = "";
+
+            posts.forEach((post) => {
+                const postElement = createPostElement(post);
+                postsContainer.appendChild(postElement);                
+            });
+
+        } catch(error){
+            postsContainer.innerHTML = '<p>Error loading posts</p>';
+        }
+    }
+
+    function createPostElement(post){
+        const article = document.createElement("article");
+        article.className = "post-card";
+
+        const title = document.createElement("h2");
+        title.textContent = post.title;
+
+        const body = document.createElement("p");
+        body.className = "post-body";
+        body.textContent = post.body;
+
+        article.appendChild(title);
+        article.appendChild(body);
+
+        return article;
+    }
+
+    fetchPosts();
+});
